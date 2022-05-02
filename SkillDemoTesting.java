0b5fa3bd3e0d95cff2c5611e9261d72a69893c8a
anhongalk <anhong@ucsd.edu>
2022-05-01 19:58:29 -0700

    Update SkillDemoTesting.java

--- SkillDemoTesting.java
+++ SkillDemoTesting.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 public class SkillDemoTesting{
     @Test
     public void testing(){
-        Assert(true, SkillDemo.EvenOrOdd(2));
+        assertEquals(true, SkillDemo.EvenOrOdd(2));
         //test case will succeed !!
     }
 }
