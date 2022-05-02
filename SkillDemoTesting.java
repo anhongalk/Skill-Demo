6ac2b1c238906f09d17327e27fad374f01bd5590
anhongalk <anhong@ucsd.edu>
2022-05-01 19:57:26 -0700

    Test case succeed!

---
+++ SkillDemoTesting.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SkillDemoTesting{
+    @Test
+    public void testing(){
+        Assert(true, SkillDemo.EvenOrOdd(2));
+        //test case will succeed !!
+    }
+}
